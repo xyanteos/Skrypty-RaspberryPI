3458fd509ac5f7d583d36cf27f1c5d35646b55e5
xyanteos <xyanteos@gmail.com>
2021-01-19 18:05:46 +0000

    edycja instalatora

--- zainstalujBiblioteki.sh
+++ zainstalujBiblioteki.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update -y && apt-get --yes --force-yes install python3 python3-setuptools python3-pip python3-dev libffi-dev build-essential python-openssl && sudo pip3 install adafruit-circuitpython-dht && python3 -m venv .env && source .env/bin/activate && pip3 install adafruit-circuitpython-dht
+apt-get update -y && apt-get --yes --force-yes install python3 python3-setuptools python3-pip python3-dev libffi-dev build-essential python-openssl && sudo pip3 install adafruit-circuitpython-dht && python3 -m venv .env && source .env/bin/activate && pip3 install adafruit-circuitpython-dht && echo "Oprogramowanie zainstalowane pomyślnie. Aby rozpocząć pracę proszę wpisać ./wyslijNaServer"
