0c89e5e99751db57ef4d7d0ab7750623a115754d
xyanteos <xyanteos@gmail.com>
2021-01-20 16:34:05 +0000

    male zmiany w instalatorze

--- zainstalujBiblioteki.sh
+++ zainstalujBiblioteki.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-apt-get update -y && apt-get --yes --force-yes install python3 python3-setuptools python3-pip python3-dev libffi-dev build-essential python-openssl && sudo pip3 install adafruit-circuitpython-dht && python3 -m venv .env && source .env/bin/activate && pip3 install adafruit-circuitpython-dht && echo "Oprogramowanie zainstalowane pomyślnie. Aby rozpocząć pracę proszę wpisać ./wyslijNaServer"
+##Skrypt ten odpowiedzialny jest za instalowanie odpowiednich bibliotek niezbednych do pobierania informacji z czujnika temp/wilg.
+apt-get update -y && apt-get --yes --force-yes install python3 python3-setuptools python3-pip python3-dev libffi-dev build-essential python-openssl && sudo pip3 install adafruit-circuitpython-dht && pip3 install pika && echo "Oprogramowanie zainstalowane pomyślnie. Aby rozpocząć pracę proszę wpisać ./wyslijNaServer"
